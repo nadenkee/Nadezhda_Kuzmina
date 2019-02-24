3e20968e46b2b039659c37fef4032184acc3b574
nadenkee <nadenkee@ya.ru>
2019-02-24 19:51:36 +0300

    hw2 - corrected

--- src/test/java/homeworks/hw2/ex1/AssertWithDataProvider.java
+++ src/test/java/homeworks/hw2/ex1/AssertWithDataProvider.java
@@ -1,12 +1,5 @@
 package homeworks.hw2.ex1;
 
-/*
-TASK
-Exercise 1:
-1.	Develop a dedicated test for asserting 4 texts below 4 pictures on the Index Page
- -https://epam.github.io/JDI/index.html. The test must be developed with help of the DataProvider.
- Run it in the parallel by methods through the configuring parameters in a @DataProvider annotation.
- */
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,8 +14,8 @@ import static org.testng.Assert.assertEquals;
 
 public class AssertWithDataProvider {
 
+    private WebDriver driver = new ChromeDriver();
     String WEB_ADDR = "https://epam.github.io/JDI/index.html";
-    WebDriver driver = new ChromeDriver();
 
     @BeforeClass
     public void BeforeClass() {
@@ -41,23 +34,18 @@ public class AssertWithDataProvider {
                 "(about 20 internal and\n" +
                 "some external projects),\n" +
                 "wish to get more…";
-//        String xPathOne = "/html/body/div/div[2]/main/div[2]/div/div[1]/div/span";
-//        String xPathTwo = "/html/body/div/div[2]/main/div[2]/div/div[2]/div/span";
-//        String xPathThree = "/html/body/div/div[2]/main/div[2]/div/div[3]/div/span";
-//        String xPathFour = "/html/body/div/div[2]/main/div[2]/div/div[4]/div/span";
 
-        int indexOne = 1;
-        int indexTwo = 2;
-        int indexThree = 3;
-        int indexFour = 4;
-
-        return new Object[][]{{indexOne, firstText}, {indexTwo, secondText}, {indexThree, thirdText}, {indexFour, forthText}};
+        return new Object[][]{
+                {1, firstText},
+                {2, secondText},
+                {3, thirdText},
+                {4, forthText}};
     }
 
     @Test(dataProvider ="correctData")
     public void SimpleTest (int index, String text) {
         driver.navigate().to(WEB_ADDR);
-        driver.manage().window().maximize(); //развернули окно браузера максимально
+        driver.manage().window().maximize();
         assertEquals(driver.findElement(By.xpath("/html/body/div/div[2]/main/div[2]/div/div["+index+"]/div/span")).getText(), text);
     }
 
